98f7d359d9aacd5fc42d05b942a9004ed75139e4
Felipe Ferreira <felipeferreira@primecase.com.br>
2021-07-02 01:22:04 -0300

    Adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import Routes from './routes/disparaEmail'
+import Routes from './routes/disparaEmail';
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(Routes);
 app.listen(3333,()=>{console.log('Server started....')});
